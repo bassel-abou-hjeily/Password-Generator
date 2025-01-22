4ef3a56711027e9301c2474f16735f3e2f7c14ab
basselabouhjeily <basselabouhjeily04@gmail.com>
2025-01-22 08:44:06 +0200

    Add files via upload

--- router/passwordRoutes.js
+++ router/passwordRoutes.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const passwordController = require('../controllers/passwordController');
 
+// Define the route for password generation
+router.get('/', (req, res) => {
+    res.render('index', { result: null });
+});
+
+router.post('/generate', passwordController.generateAndSavePassword);
+
+// Export the router
+module.exports = router;
