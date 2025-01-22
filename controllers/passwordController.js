e9dee959195df9b66cbfb1fd8c40a72c55308195
basselabouhjeily <basselabouhjeily04@gmail.com>
2025-01-22 08:46:49 +0200

    Add files via upload

--- controllers/passwordController.js
+++ controllers/passwordController.js
@@ -1,1 +1,18 @@
+const Password = require('../modules/Password');
+const generatePassword = require('../utils/passwordGenerator');
+
+// Generate and save password
+exports.generateAndSavePassword = async (req, res) => {
+    const length = parseInt(req.body.length, 10);
+    const password = generatePassword(length);
+
+    try {
+        const newPassword = new Password({ password });
+        await newPassword.save();
+        res.render('index', { result: password });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error saving password');
+    }
+};
 
